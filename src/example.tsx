9db31b86fbf8f8b1d8e7075b5f32dbb5f3e25a00
Arjan Scherpenisse <arjan@scherpenisse.net>
2020-01-02 11:57:06 +0100

    Update README

--- src/example.tsx
+++ src/example.tsx
@@ -3,10 +3,6 @@ import * as ReactDOM from "react-dom";
 
 import SentenceEditor from './components/SentenceEditor';
 
-// import { Hello } from "./components/Hello";
-
-console.log(document.getElementById("root"))
-
 const sentence = {
   id: 'f9b44dee-1562-448c-8b37-1cd3b2399160',
   data: [
