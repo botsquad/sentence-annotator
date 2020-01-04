847fff432bf043541e94f48c01d6c17b3a3f09cb
Arjan Scherpenisse <arjan@scherpenisse.net>
2020-01-04 21:35:27 +0100

    Fix drag interaction by setting the key

--- src/components/SentenceEditor.tsx
+++ src/components/SentenceEditor.tsx
@@ -108,7 +108,7 @@ export default class SentenceEditor extends React.Component<Props, State> {
         {this.props.value.data.map((value, index) =>
           value.userDefined
           ? <LabeledToken
-              key={index}
+              key={`${value.alias}${value.meta}`}
               index={index}
               token={value}
               selected={index === this.state.selectedToken}
